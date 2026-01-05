288cca5f6b921ff220a9cd4194860619e35d05c6
bamejiamedina <mejiamedinabryan@gmail.com>
2026-01-04 22:39:32 -0500

    add sample puzzle

--- src/utils/gridUtils.ts
+++ src/utils/gridUtils.ts
@@ -2,12 +2,40 @@ import { CellData } from "../components/cell/types";
 
 export const DIMENSIONS = 9;
 
+const samplePuzzle: number[][] = [
+  [0, 0, 0, 0, 0, 0, 7, 0, 3],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 3, 0, 0],
+  [0, 2, 0, 2, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 4, 0],
+  [0, 0, 5, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [3, 0, 3, 0, 0, 0, 0, 0, 0],
+];
+
 export const createBlankGrid = (): CellData[][] => {
   return Array.from({ length: DIMENSIONS }, (_, rowIndex) =>
     Array.from({ length: DIMENSIONS }, (_, columnIndex) => ({
       rowIndex,
       columnIndex,
       color: "white",
+    }))
+  );
+};
+
+export const createSamplePuzzle = (): CellData[][] => {
+  return samplePuzzle.map((row, rowIndex) =>
+    row.map((number, columnIndex) => (
+    number === 0 ? {
+      rowIndex,
+      columnIndex,
+      color: "white"
+    } : {
+      rowIndex,
+      columnIndex,
+      color: "white",
+      number: number
     }))
   );
 };
