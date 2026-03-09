9a7bd9b2c04b47849c9d9978ed01ac9d88f8dd3f
Amadeus Kang <amadeuskang@gmail.com>
2026-03-08 23:36:10 -0400

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import Grid from "./components/grid/Grid";
 import Instructions from './components/instructions/Instructions';
 import './App.css';
